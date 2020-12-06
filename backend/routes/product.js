01363e67fc39e5a1ddf8214a9f243267fbd2eac4
Ripunjai <ripunjai2@gmail.com>
2020-12-06 17:30:28 +0530

    Other CRUD routes for product

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { getUserById } = require('../controllers/user');
 const { isSignedIn, isAuthenticated, isAdmin } = require('../controllers/auth');
-const { getProductById, createProduct } = require('../controllers/product');
+const { getProductById, createProduct, getProduct, photo, updateProduct, deleteProduct, getAllProducts, getAllUniqueCategories } = require('../controllers/product');
 
 //params
 router.param('userId', getUserById);
@@ -11,5 +11,11 @@ router.param('productId', getProductById);
 
 //Routes
 router.post('/product/create/:userId', isSignedIn, isAuthenticated, isAdmin, createProduct);
+router.get('/product/:productId', getProduct);
+router.get('/product/photo/:productId', photo);
+router.get('/product/products', getAllProducts);
+router.delete('/prouct/:productId/:userId', isSignedIn, isAuthenticated, isAdmin, deleteProduct);
+router.put('/prouct/:productId/:userId', isSignedIn, isAuthenticated, isAdmin, updateProduct);
+router.get('/product/categories', getAllUniqueCategories);
 
 module.exports = router;
