7186b66ce77bf493a3c76f545d70a42c529eff88
Ripunjai <ripunjai2@gmail.com>
2020-12-23 19:07:34 +0530

    Done with the authentication

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import '../styles.css';
-import { API } from '../backend';
-import Base from './Base';
+import React from "react";
+import "../styles.css";
+import { API } from "../backend";
+import Base from "./Base";
 
 export default function Home() {
-  console.log('API IS', API);
+  console.log("API IS", API);
 
   return (
-    <Base title='Home Page'>
-      <div className='row'>
-        <div className='col-4'>
-          <button className='btn btn-success'>TEST</button>
+    <Base title="Home Page" description="Welcome to the Tshirt Store">
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
         </div>
-        <div className='col-4'>
-          <button className='btn btn-success'>TEST</button>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
         </div>
-        <div className='col-4'>
-          <button className='btn btn-success'>TEST</button>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
         </div>
       </div>
     </Base>
